6d3ba59c64ceec9051f52423dcf8605595502615
AliNaqvi97 <AliNaqvi1397@gmail.com>
2018-12-10 05:35:02 -0600

    Needed to ask for location permissions, also added favorites button to menu as well

--- app/src/main/java/com/alihnaqvi/findmymeal/ResultsActivity.kt
+++ app/src/main/java/com/alihnaqvi/findmymeal/ResultsActivity.kt
@@ -1,17 +1,26 @@
 package com.alihnaqvi.findmymeal
 
+import android.Manifest
 import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Location
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
 import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentManager
 import android.support.v4.app.FragmentPagerAdapter
+import android.support.v4.content.ContextCompat
+import android.support.v4.view.PagerAdapter
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
+import android.view.Menu
 import android.view.MenuItem
 import android.view.View
 import android.view.animation.AnimationUtils
 import android.widget.Toast
+import com.google.android.gms.location.LocationServices
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.Disposable
 import io.reactivex.schedulers.Schedulers
@@ -25,17 +34,41 @@ class ResultsActivity : AppCompatActivity() {
         setContentView(R.layout.activity_results)
         this.title = intent.getStringExtra("category")
 
-        val pagerAdapter = PagerAdapter(supportFragmentManager)
-        viewpager.adapter = pagerAdapter
-
         tabLayout.setupWithViewPager(viewpager)
         supportActionBar?.elevation = 0f
+
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+            ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION), 0)
+        } else {
+            val pagerAdapter = PagerAdapter(supportFragmentManager)
+            viewpager.adapter = pagerAdapter
+
+        }
     }
 
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        if (requestCode == 0) {
+            if (grantResults.isNotEmpty() && grantResults[0]== PackageManager.PERMISSION_GRANTED && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+                val pagerAdapter = PagerAdapter(supportFragmentManager)
+                viewpager.adapter = pagerAdapter
+            }
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_favorites, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+
     override fun onOptionsItemSelected(item: MenuItem?): Boolean {
-        if (item?.itemId == android.R.id.home) {
-            finish()
-            return true
+        when (item?.itemId) {
+            android.R.id.home -> {
+                finish()
+                return true
+            }
+            R.id.open_favorites -> startActivity(Intent(this, FavoritesActivity::class.java))
         }
         return super.onOptionsItemSelected(item)
     }
