cb8d53cedaa06d88770dd8ce4139e2e0c355dd4b
AliNaqvi97 <AliNaqvi1397@gmail.com>
2018-12-10 05:35:39 -0600

    Checking for internet connection

--- app/src/main/java/com/alihnaqvi/findmymeal/RecipesFragment.kt
+++ app/src/main/java/com/alihnaqvi/findmymeal/RecipesFragment.kt
@@ -1,6 +1,9 @@
 package com.alihnaqvi.findmymeal
 
 import android.app.Activity
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkInfo
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v7.widget.LinearLayoutManager
@@ -15,33 +18,40 @@ import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.fragment_recipes.*
 import kotlinx.android.synthetic.main.fragment_recipes.view.*
 
-class RecipesFragment() : Fragment() {
-    private lateinit var resultsRecyclerView: RecyclerView
+class RecipesFragment : Fragment() {
+    private lateinit var recipesRecyclerView: RecyclerView
     private lateinit var recipesAdapter: RecipesAdapter
     private lateinit var param: String
 
-    private val apiService by lazy { ApiService.createService() }
+    private val apiService by lazy { ApiService.createService(ApiService.BASE_MEAL_URI) }
     private var disposable: Disposable? = null
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_recipes, container, false)
-        resultsRecyclerView = view.recyclerview_recipes
-        resultsRecyclerView.layoutManager = LinearLayoutManager(activity, LinearLayoutManager.VERTICAL, false)
+        recipesRecyclerView = view.recyclerview_recipes
+        recipesRecyclerView.layoutManager = LinearLayoutManager(activity, LinearLayoutManager.VERTICAL, false)
         return view
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+
+        retry_button.setOnClickListener {
+            checkInternetConnection()
+        }
+
         param = activity!!.intent.getStringExtra("category")
         recipesAdapter = RecipesAdapter(activity as Activity)
-        resultsRecyclerView.adapter = recipesAdapter
-        getMeals(param)
+        recipesRecyclerView.adapter = recipesAdapter
+
+        checkInternetConnection()
     }
 
     private fun getMeals(category: String) {
-        val observable =
-            if (category == "Seafood") apiService.fetchMealsByCategory(category)
-            else apiService.fetchMealsByArea(category)
+        val observable = when (category) {
+            "Seafood", "Vegan", "Vegetarian" -> apiService.fetchMealsByCategory(category)
+            else -> apiService.fetchMealsByArea(category)
+        }
 
         disposable =
                 observable
@@ -57,6 +67,23 @@ class RecipesFragment() : Fragment() {
     private fun showMeals(meals: ArrayList<Models.Meal>) {
         progress_circular.visibility = View.GONE
         recipesAdapter.addAll(meals)
+    }
+
+    private fun checkInternetConnection() {
+        progress_circular.visibility = View.VISIBLE
+        val cm = context?.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val activeNetwork: NetworkInfo? = cm.activeNetworkInfo
+        val isConnected: Boolean = activeNetwork?.isConnectedOrConnecting == true
+
+        if (isConnected) {
+            empty_view.visibility = View.GONE
+            retry_button.visibility = View.GONE
+            getMeals(param)
+        } else {
+            progress_circular.visibility = View.GONE
+            empty_view.visibility = View.VISIBLE
+            retry_button.visibility = View.VISIBLE
+        }
     }
 
     override fun onDestroy() {
